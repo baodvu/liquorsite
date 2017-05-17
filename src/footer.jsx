fe85889ca5bf888cc3fac98005ccb88908d5d832
Bao Vu <baodvu@gmail.com>
2017-05-17 01:15:00 -0400

    Added react router and page skeleton

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,9 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Footer extends Component {
-    render(){
-        return (<h1>Footer</h1>);
-    }
+class Footer extends React.Component {
+  render(){
+    return (
+      <div id="footer-wrapper">
+        <section id="footer" className="container">
+          <div className="row">
+            <div className="10u 12u(mobile)">
+              <p>Ace Liquor & Wine | Liquor Store | Nanuet, NY</p>
+              <p>845-623-2157</p>
+            </div>
+            <div className="2u 12u(mobile)">
+              <ul className="social">
+                <li><a className="icon fa-facebook" href="#">
+                <span className="label">Facebook</span></a></li>
+                <li><a className="icon fa-twitter" href="#">
+                <span className="label">Twitter</span></a></li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>);
+  }
 }
 
 export default Footer
--- src/header.jsx
+++ src/header.jsx
@@ -1,9 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Header extends Component {
-    render(){
-        return (<h1>Ace Liquor and Wine</h1>);
-    }
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePath: 'wine'
+    };
+  }
+
+  render(){
+    return (
+      <div id="header-wrapper">
+        <div id="header">
+          <h1><a href="/">Ace Liquor & Wine</a></h1>
+          <nav id="nav">
+            <ul>
+              <li className={(this.state.activePath === "home") ? "current" : ""}><a href="/">Home</a></li>
+              <li className={(this.state.activePath === "wine") ? "current" : ""}><a href="wine">Wine</a></li>
+              <li><a href="liquor">Liquor</a></li>
+              <li><a href="contact">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>);
+  }
 }
 
 export default Header
