a52c0164fa77682fa28179750aa111fa44151de9
Bao Vu <baodvu@gmail.com>
2017-05-20 04:58:22 -0400

    switch to prod env

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,24 @@ var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'src');
 
+var dev_config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        exclude: /node_modules/,
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      }
+    ]
+  },
+}
+
 var config = {
   entry: APP_DIR + '/index.jsx',
   output: {
@@ -19,7 +37,18 @@ var config = {
         loader: 'babel-loader?presets[]=es2015&presets[]=react'
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // A common mistake is not stringifying the "production" string.
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
 };
 
 module.exports = config;
